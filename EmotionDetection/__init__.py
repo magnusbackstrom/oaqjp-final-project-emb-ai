034d22f032796569f8d054dc36763750fade1faa
magnusbackstrom <magnus_beckstrom@hotmail.com>
2025-05-03 15:23:35 -0400

    First commit

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 from .emotion_detection import emotion_detector
+=======
+from . import emotion_detection
+>>>>>>> 690a5ac80d891043653b5c3a5055e8dd908f74a1
--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+<<<<<<< HEAD
 import json
  
 def emotion_detector(text_to_analyze):
@@ -11,3 +12,12 @@ def emotion_detector(text_to_analyze):
      dominant_emotion = max(emotions.items(), key=lambda x: x[1])
      emotions['dominant_emotion'] = dominant_emotion[0]
      return emotions
+=======
+def emotion_detector(text_to_analyse):
+    url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json=input_json, headers=header)
+    
+    return response.text
+>>>>>>> 690a5ac80d891043653b5c3a5055e8dd908f74a1
